448acd688b6556eacbdb58abe80c4816eec145f8
Mary <korikokoro@mail.ru>
2021-01-29 15:28:18 +0500

    json

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const DIST_DIR = path.join(__dirname, '/dist');
+const DIST_DIR = path.join(__dirname, '/dist/GGJ');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
-});
+}); 
 
 const PORT = process.env.PORT || 3000;
 
