a5b9992875f75cd1d725447c0236cda5f400e877
Mary <korikokoro@mail.ru>
2021-01-29 17:51:42 +0500

    test

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const DIST_DIR = path.join(__dirname, '/dist/GGJ');
+const DIST_DIR = path.join(__dirname, 'dist');
+app.use(express.static(DIST_DIR));
+// app.use(express.static(path.join(__dirname, 'public')));
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
-app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
 }); 
